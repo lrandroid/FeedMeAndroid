c2db3941b53ee01d23e7a20e6179b46b0fe0fe39
Wesley Kim <wesleyjkim@gmail.com>
2012-07-22 01:48:28 -0700

    Added stripe and gson

---
+++ FeedMeAndroid/src/com/android/feedmeandroid/exception/AuthenticationException.java
@@ -0,0 +1,12 @@
+package com.android.feedmeandroid.exception;
+
+public class AuthenticationException extends StripeException {
+
+
+	public AuthenticationException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ FeedMeAndroid/src/com/android/feedmeandroid/model/NextRecurringCharge.java
@@ -0,0 +1,22 @@
+package com.android.feedmeandroid.model;
+
+public class NextRecurringCharge extends StripeObject {
+	Integer amount;
+	String date;
+	
+	public Integer getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
