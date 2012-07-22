58b055c32e9ea886da65cd0be7ffa22b1240772b
Wesley Kim <wesleyjkim@gmail.com>
2012-07-22 01:29:40 -0700

    added stripe sdk

---
+++ stripe-java/src/main/java/com/stripe/Stripe.java
@@ -0,0 +1,8 @@
+package com.stripe;
+
+public abstract class Stripe
+{
+	public static final String API_BASE = "https://api.stripe.com/v1";
+	public static final String VERSION = "1.1.1";
+	public static String apiKey;
+}
